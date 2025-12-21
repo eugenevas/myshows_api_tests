ae9cd619244f8da839c8b870012704865633f096
Eugene Vasilyev <eugene.vas@yandex.ru>
2025-12-21 17:22:35 +0500

    fixed files

--- fixtures/api_fixtures.py
+++ fixtures/api_fixtures.py
@@ -18,9 +18,9 @@ def add_episodes():
 
     # Тестовые данные - на входе статус в API-значении
     test_series = [
-        {"id": 6, "name": "Наруто", "photo": "url1", "rating": 9, "status": "Смотрю","review": "Отличный сериал"},
-        {"id": 7, "name": "Мандалорец", "photo": "url2", "rating": 9, "status": "Посмотрел", "review": "Эпично"},
-        {"id": 8, "name": "Союз спасения. Время гнева", "photo": "url3", "rating": 8, "status": "Буду смотреть", "review": "Интересный сюжет"}
+        {"id": 6, "name": "Наруто", "photo": "https://media.myshows.me/shows/760/3/e8/3e8e697187b0fdb49941ecf22db5b9b3.jpg", "rating": 9, "status": "Смотрю","review": "Отличный сериал"},
+        {"id": 7, "name": "Мандалорец", "photo": "https://media.myshows.me/shows/760/6/61/66124cb92abfa1ae993d66f2e80463fc.jpg", "rating": 9, "status": "Посмотрел", "review": "Эпично"},
+        {"id": 8, "name": "Союз спасения. Время гнева", "photo": "https://media.myshows.me/shows/760/c/64/c6463ee65ae3aeaf071af0f502befc04.jpg", "rating": 8, "status": "Буду смотреть", "review": "Интересный сюжет"}
     ]
 
 
