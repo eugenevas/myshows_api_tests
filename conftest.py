333af2b8e9920fadb38fa6be7c4e2e6b9c35b8bd
eugenevas <48223896+eugenevas@users.noreply.github.com>
2026-01-12 12:43:02 +0500

    fixed api_config (#10)
    * fixed api_config, api_fixture, test.
    added api_helpers, bd_config, db_helpers, delete_series.sql, insert_series.sql, series_for_testing.py
    
    * added db_config, fixed api_config
    
    * Добавил .env в .gitignore и убрал из отслеживания
    
    * fixed conftest.py
    
    * fixed api_config.py

--- conftest.py
+++ conftest.py
@@ -1,2 +1,4 @@
+from dotenv import load_dotenv
+load_dotenv() # эти две строки нужны чтобы подгрузился перед тестами файл .env иначе не находит его автоматом
 
 pytest_plugins = ["fixtures.api_fixtures"]
